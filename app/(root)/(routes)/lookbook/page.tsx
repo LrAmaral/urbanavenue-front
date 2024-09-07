1f3c37e9d67a94bf80d944773e4f01c642a2f0d5
LrAmaral <lucasamaralap@outlook.com>
2024-09-07 12:39:54 -0300

    :wrench: fix

--- app/(root)/(routes)/lookbook/page.tsx
+++ app/(root)/(routes)/lookbook/page.tsx
@@ -1,6 +1,7 @@
 import { Metadata } from "next";
-import Gallery from "./components/gallery";
 import { Wrapper } from "@/components/Custom/wrapper";
+import Image from "next/image";
+import { images } from "@/lib/images";
 
 export const metadata: Metadata = {
   title: "takeatrip | UrbanAvenue®",
@@ -14,7 +15,25 @@ const Lookbook = () => {
         <div>
           <div className="flex flex-col justify-center items-center">
             <div className="w-full text-center space-y-4">
-              <Gallery />
+              <div className="flex w-full mt-4 justify-center items-center flex-col">
+                <div className="grid grid-cols-2 md:grid-cols-3 gap-6 md:gap-12 justify-items-center items-center rounded-2xl">
+                  {images.map(({ id, src }) => (
+                    <div
+                      key={id}
+                      className="cursor-pointer hover:scale-105 transition ease-in-out duration-500"
+                    >
+                      <Image
+                        src={src}
+                        width={1920}
+                        height={1080}
+                        loading="lazy"
+                        alt="image"
+                        className="rounded-2xl pointer-events-none"
+                      />
+                    </div>
+                  ))}
+                </div>
+              </div>
             </div>
           </div>
         </div>
