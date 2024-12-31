46bf004a44178b956780d4c29b817b7f265eb026
LrAmaral <lucasamaralap@outlook.com>
2024-12-31 11:23:51 -0300

    fix

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,10 +1,11 @@
 import { SignIn } from "@clerk/nextjs";
+import Link from "next/link";
 
 export default function SignInPage() {
   return (
     <div className="flex flex-col items-center space-y-6">
       <p className="text-gray-700 text-center text-xl">Access your account:</p>
-      <SignIn />
+      <SignIn signUpUrl="/sign-up" />
     </div>
   );
 }
